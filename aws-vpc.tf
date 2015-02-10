d4f46d5a157bcf04872997b938617f1e00bed63a
Christopher Coté <Christopher Cot>
2015-02-10 11:15:22 -0600

    adds access through bastion

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -127,6 +127,20 @@ resource "aws_security_group" "bastion" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	ingress {
+		from_port = 443
+		to_port = 443
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
 	tags {
 		Name = "${var.aws_vpc_name}-bastion"
 	}
