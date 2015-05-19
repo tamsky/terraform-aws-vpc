b95a1554fb277d723985b201e626771664d56531
Marc Tamsky <tamsky@users.noreply.github.com>
2015-05-19 15:22:04 -0700

    add an internal instance, security group, and allow internal ping of bastion.

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -37,9 +37,27 @@ resource "aws_instance" "bastion" {
 	}
 }
 
+output "aws_instance_bastion_public_ip" {
+       value = "${aws_instance.bastion.public_ip}"
+}
+
 resource "aws_eip" "bastion" {
 	instance = "${aws_instance.bastion.id}"
 	vpc = true
+}
+
+resource "aws_instance" "internal" {
+	ami = "${lookup(var.aws_ubuntu_ami, var.aws_region)}"
+	instance_type = "t2.small"
+	key_name = "${var.aws_key_name}"
+	security_groups = ["${aws_security_group.internal.id}"]
+	subnet_id = "${aws_subnet.app.id}"
+# only on NAT gateways:
+#	associate_public_ip_address = true
+#	source_dest_check = false
+	tags {
+		Name = "internal"
+	}
 }
 
 # Public subnets
@@ -115,9 +133,13 @@ resource "aws_route_table_association" "app-private" {
 	route_table_id = "${aws_route_table.private.id}"
 }
 
+##
+# Security Groups
+##
+
 resource "aws_security_group" "bastion" {
 	name = "bastion"
-	description = "Allow SSH traffic from the internet"
+	description = "Allow SSH traffic, and tcp/NAT from the internet"
 	vpc_id = "${aws_vpc.default.id}"
 
 	ingress {
@@ -125,14 +147,22 @@ resource "aws_security_group" "bastion" {
         to_port = 65535
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
-    }
+        }
+
+	ingress {
+        from_port = -1
+        to_port = -1
+        protocol = "icmp"
+        self = true
+        security_groups = [ "${aws_security_group.internal.id}" ]
+        }
 
 	egress {
         from_port = 0
         to_port = 65535
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
-    }
+        }
 
 	tags {
 		Name = "${var.aws_vpc_name}-bastion"
@@ -140,6 +170,35 @@ resource "aws_security_group" "bastion" {
 
 }
 
+resource "aws_security_group" "internal" {
+	name = "internal"
+	description = "Allow traffic within our VPC subnet"
+	vpc_id = "${aws_vpc.default.id}"
+
+	ingress {
+          from_port = 0
+          to_port = 0
+          protocol = "-1"
+          cidr_blocks = [ "${aws_vpc.default.cidr_block}" ]
+        }
+
+        egress {
+          from_port = 0
+          to_port = 0
+          protocol = "-1"
+          cidr_blocks = [ "${aws_vpc.default.cidr_block}" ]
+        }
+
+	tags {
+		Name = "${var.aws_vpc_name}-internal"
+	}
+
+}
+
 output "aws_security_group_bastion_id" {
   value = "${aws_security_group.bastion.id}"
+}
+
+output "aws_security_group_internal_id" {
+  value = "${aws_security_group.internal.id}"
 }
